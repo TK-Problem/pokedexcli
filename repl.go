453d57b75de6a381c7064ecd9eadfed8c4888197
Tomas <t.uzdavinys@gmail.com>
2026-08-15 10:48:08 +0300

    CH1L4.

--- repl.go
+++ repl.go
@@ -1,6 +1,28 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+// startRepl reads commands from stdin until the process is interrupted.
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		fmt.Printf("Your command was: %s\n", words[0])
+	}
+}
 
 // cleanInput lowercases the text and splits it into words on any run of
 // whitespace. Leading and trailing whitespace is discarded.
